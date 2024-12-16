a3797dbbedb67f84d223a7ef4fc1b3a263a142c3
K M AKASH <42842880+Akash1624@users.noreply.github.com>
2024-12-16 17:27:14 +0530

    Add files via upload

---
+++ DAC/SquareWave_Generator/dac.c
@@ -0,0 +1,28 @@
+/*
+This program generates square wave
+*/
+/****************************************************
+
+Submission Details:
+Name: K M AKASH, RAKESH SG, ASHWINI JAYSHETTY
+Reg No: 241039022, 241039034, 241039021
+Branch: Embedded Systems
+File: DAC file
+
+Clock Settings:
+	FOSC	>>	12MHz (onboard)
+	PLL		>>	M=5, P=2
+	CCLK	>>  60MHz
+	PCLK	>>  15MHz 
+		
+Hardware Setup:-
+P0.25 -> Oscilloscope (A)
+
+*****************************************************/
+
+#include "header.h"   // Include the header for function declarations
+
+// Initialize DAC on P0.25
+void initDAC(void) {
+    PINSEL1 |= (1 << 19);  // Configure P0.25 as DAC output (bits 18-19 in PINSEL1)
+}
