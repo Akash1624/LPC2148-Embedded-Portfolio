cb5203bccfb6850906c78b50dd6f2137a948f077
K M AKASH <42842880+Akash1624@users.noreply.github.com>
2024-12-16 17:28:03 +0530

    Update dac.c

--- DAC/TriangularWave_Generator/dac.c
+++ DAC/TriangularWave_Generator/dac.c
@@ -1,12 +1,10 @@
 /*
-This program generates Ramp Wave
+This program generates Triangle wave
 */
 /****************************************************
 
 Submission Details:
-Name: K M AKASH, RAKESH SG, ASHWINI JAYSHETTY
-Reg No: 241039022, 241039034, 241039021
-Branch: Embedded Systems
+Name: K M AKASH
 File: DAC file
 
 Clock Settings:
@@ -19,20 +17,10 @@ Hardware Setup:-
 P0.25 -> Oscilloscope (A)
 
 *****************************************************/
-
-#include "header.h"   // Include the header for function declarations
+#include "header.h"
 
 // Initialize DAC on P0.25
 void initDAC(void) {
     PINSEL1 |= (1 << 19);  // Configure P0.25 as DAC output (bits 18-19 in PINSEL1)
 }
 
-// Simple millisecond delay function
-void delay_ms(unsigned int ms) {
-    unsigned int i, j;
-    for (i = 0; i < ms; i++) {
-        for (j = 0; j < 10000; j++) {
-            // Waste time to create a delay
-        }
-    }
-}
